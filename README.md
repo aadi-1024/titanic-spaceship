d7601b2398a64d1068fbd29476c0622b219e8cc1
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-06-12 16:29:15 +0530

    m

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ima make separate repos for each project now mm
 
 got 0.79 as a baseline, not bad
-#1 on leaderboard is 0.32 so i hope to get atleast 0.85
+#1 on leaderboard is 0.92 so i hope to get atleast 0.85
+
+hmm adding total_expenses reduces the accuracy ever so slightly, somehow using gridsearch makes it worse??
---
+++ main.py
@@ -0,0 +1,70 @@
+#mosty copied from baseline.py lol
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.base import TransformerMixin, BaseEstimator
+import numpy as np
+
+class AddFeature(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        return self
+
+    def transform(self, X, y=None):
+        a = X[:,1] + X[:,2] + X[:,3] + X[:,4] + X[:,5]
+        return np.c_[X, a]
+
+num_pipeline = Pipeline([
+    ('inputing', SimpleImputer(strategy='median')),
+    ('adding', AddFeature()),
+    ('scaling', StandardScaler()),
+])
+
+cat_pipeline = Pipeline([
+    ('encoding', OneHotEncoder()),
+    ('imputer', SimpleImputer(strategy='median'))
+])
+
+pipeline = ColumnTransformer([
+    ('drop', 'drop', ['PassengerId', 'Cabin', 'Destination', 'Name']),
+    ('num', num_pipeline, ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']),
+    ('xat', cat_pipeline, ['CryoSleep', 'VIP', 'HomePlanet'])
+])
+
+def main():
+    train = pd.read_csv('train.csv')
+    train_y = train.Transported
+
+    train.drop('Transported', axis=1)
+    train = pipeline.fit_transform(train)
+
+    params = {
+        'n_estimators': [10, 50, 100, 250, 500, 1000],
+        'max_depth': [None, 10, 50, 100, 250],
+    }
+
+    gs = GridSearchCV(RandomForestClassifier(), params, n_jobs=-1, scoring='f1')
+    gs.fit(train, train_y)
+    # gs. max_depth = 10, n = 250
+
+    # rt = RandomForestClassifier(n_estimators=250, max_depth=10, n_jobs=-1)
+    # train = pipeline.fit_transform(train)
+
+    # rt.fit(train, train_y)
+
+    test = pd.read_csv('test.csv')
+    pid = test.PassengerId
+
+    test = pipeline.transform(test)
+    pred = gs.predict(test)
+    cc = pd.concat([pid, pd.DataFrame(pred)], ignore_index=True, axis=1)
+    cc.to_csv('./final.csv', index=False)
+
+if __name__ == "__main__":
+    main()
